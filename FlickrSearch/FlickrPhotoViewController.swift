dba7b9115914bae05df978ae44d402a023432268
Jeff Norton <jeff12ntn@gmail.com>
2016-11-20 10:26:04 -0700

    Basic UI setup done

---
+++ FlickrSearch/FlickrPhotoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FlickrPhotoViewController.swift
+//  FlickrSearch
+//
+//  Created by Jeff Norton on 11/20/16.
+//  Copyright © 2016 JeffCryst. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoViewController: UIViewController {
+    
+    //==================================================
+    // MARK: - _Properties
+    //==================================================
+    
+    @IBOutlet weak var resultsCollectionView: UICollectionView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    //==================================================
+    // MARK: - General
+    //==================================================
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
