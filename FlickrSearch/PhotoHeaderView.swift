00cbeb13f57aed98193bbf819b48ba850732c79a
Jeff Norton <jeff12ntn@gmail.com>
2016-11-20 12:27:15 -0700

    Basics of views and view controllers done

---
+++ FlickrSearch/PhotoHeaderView.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoHeaderView.swift
+//  FlickrSearch
+//
+//  Created by Jeff Norton on 11/20/16.
+//  Copyright © 2016 JeffCryst. All rights reserved.
+//
+
+import UIKit
+
+class PhotoHeaderView: UICollectionReusableView {
+    
+    //==================================================
+    // MARK: - _Properties
+    //==================================================
+    
+    @IBOutlet weak var sectionLabel: UILabel!
+        
+}
